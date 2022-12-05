0711b11ba124d9b3bfa95a8913575568e01571e7
Potkaj.Przemyslaw <przemek0987@poczta.onet.pl>
2022-12-05 10:25:03 +0100

    a

--- SQLQuery/02_CREATE TABLE part.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE part (
-	id INT NOT NULL IDENTITY(1,1),
-	part_number VARCHAR(60) NOT NULL,
-	[weight] DECIMAL(10, 3) NOT NULL,
-	PRIMARY KEY(id)
-)
--- SQLQuery/03_CREATE TABLE price_list.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE price_list (
-	id INT NOT NULL IDENTITY(1,1),
-	part_id INT NOT NULL,
-	threshold DECIMAL(10, 3) NOT NULL,
-	price_net MONEY NOT NULL,
-	PRIMARY KEY(id),
-	FOREIGN KEY(part_id) REFERENCES part(id)
-)
